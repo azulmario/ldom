bf22925db7f36cadc25b60f85b05097dafd37693
Mario Hernández Morales <azulmario@yahoo.com.mx>
2016-03-16 14:13:45 -0600

    Plantilla inicial, utiliza un estilo de llevar al usuario por medio de tres pasos

--- server.R
+++ server.R
@@ -1,67 +1,48 @@
 library(shiny)
-library(readxl)
 library(utils)
 library(tools)
-library(plyr)
-
+library(readxl)
 source("qmaps.R")
-matricula <- NULL
-res <- NULL
 
 options(shiny.maxRequestSize = 9*1024^2)
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  jj <- ""
+  kk <- ""
+  
   output$matricula <- renderTable({
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    
     inFile <- input$file1
     
     if (is.null(inFile))
       return(NULL)
     
     #Renombrar
-    #system
-    jj <- file.path(dirname(inFile$datapath), inFile$name)
-    kk <- system(paste('mv', inFile$datapath, jj))
+    jj <<- file.path("/srv/shiny-server/docs/out", inFile$name)
+    system(paste('cp -f', inFile$datapath, jj))
     #Leer
-    matricula <<- read_excel(path = jj, sheet = input$sheet)
-    
-    #Borrar
-    system(paste('rm', jj))
-  
-    data.frame(matricula[1:10,])
+    matricula <- lee(jj, input$sheet)
+
+    data.frame(matricula[1:(min(10,length(matricula$n))),])
   })
   
   output$downloadData <- downloadHandler(
-    filename = function(){
+    filename = function() {
       paste('res-', Sys.Date(), '.csv', sep='')
     },
     content = function(file) {
-      write.csv(merge(matricula, res[c(7, 1:6)]), file, row.names = FALSE)
+      matricula <- read_excel(kk)
+      write.csv(matricula, file, row.names = FALSE)
     }
   )
 
   ntext <- eventReactive(input$goButton, {
     options(show.error.messages = FALSE)
-    numera <- "n" %in% colnames(matricula)
-    res <<- ldply(1:length(matricula$mun), function(n) {
-      # Procesa la dirección y obtiene la coordenada geográfica probable
-      c <- try(atomizar(podar(identifica(matricula[n,]))), silent = TRUE)
-      if(class(c) == "try-error") {
-        c <- data.frame(niv=-2, BM=0, cve=0, nombre=0, lat=0, lon = 0)
-      }
-      #Agrega el número de renglón
-      c$n <-ifelse(numera, matricula[n,]$n, n)
-      c
-    }, .parallel = FALSE, .progress = "time")
+    kk <<- paste('/srv/shiny-server/docs/out/', 'r', Sys.time(), '.xlsx', sep='')
+    opetus <- paste("/usr/bin/Rscript -e \"source('/srv/shiny-server/ldom/qmaps.R'); main(path = '",jj,"', sheet = ",input$sheet,", file = '",kk,"', paralelo = TRUE);\" --vanilla &", sep='') 
+    system(opetus)
     options(show.error.messages = TRUE)
-    "Terminado!"
+    paste("¡Espere a que termine el proceso! Al finalizar, utilice la carpeta de trabajo o el siguiente vínculo:")
   })
-  
+
   output$nText <- renderText({
     ntext()
   })
--- ui.R
+++ ui.R
@@ -1,17 +1,28 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("LDom"),
+  titlePanel("Localización automatizada de domicilios geográficos"),
   sidebarLayout(
     sidebarPanel(
-      h3("Localización automatizada de domicilios geográficos"),
       p("Ejemplo demostrativo de la potencialidad de localización automatizada de domicilios geográficos. Para su desarrollo se implementa una técnica de inteligencia artificial denominada árbol de decisión."),
-      p('Si quieres un archivo muestra para subir,',
-        'puede descargar la plantilla',
-        a(href = 'demo.xlsx', 'demo.xlsx'),
-        'y utilizarla para sus datos.'
-      ),
       tags$hr(),
+      h4("Notas"),
+      p("Los archivos de entrada deben tener un formato específico, primero se deben identificar los campos necesarios, según la norma técnica de domicilios geográficos de Inegi e identificar con etiquetas preestablecida."),
+      p('Puedes utilizar el archivo muestra, tomarlo como referencia, aquí puedes descargar la plantilla',
+        a(href = '../docs/demo.xlsx', 'demo.xlsx', target='_blank'), 'para utilizarla para sus datos.'),
+      p("La velocidad depende de las características de la máquina virtual, principalmente en el número de procesadores asignados."),
+      tags$hr(),
+      h4("Referencias"),
+      p('Programa de captura de domicilios con adecuaciones para visualizarlo en un mapa:',
+        a(href = '../dom', '/dom', target='_blank'), '.'),
+      p('Reporte de características del programa automatizado:',
+        a(href = '../reporte', '/reporte', target='_blank'), '.'),
+      p('Carpeta de trabajo:',
+        a(href = '../docs/out', '/out', target='_blank'), '.')
+    ),
+    mainPanel(
+      h3("Primer paso"),
+      p("El primer paso es cargar el archivo de direcciones."),
       numericInput("sheet", 
                    label = p("Hoja a leer, un número entero (la posición de la hoja). Por defecto es la primera hoja."), 
                    value = 1, min = 1, step = 1),
@@ -24,17 +35,15 @@ shinyUI(fluidPage(
                   '.xlsx'
                 )
       ),
+      tableOutput('matricula'),
       tags$hr(),
+      h3("Segundo paso"),
       actionButton('goButton','Procesamiento por lote'),
       tags$hr(),
+      h3("Tercer paso"),
       verbatimTextOutput("nText"),
       tags$hr(),
       downloadButton('downloadData', 'Download')
-    ),
-    mainPanel(
-      h2("Primer paso"),
-      p("El primer paso es cargar el archivo de direcciones."),
-      tableOutput('matricula')
     )
   )
 ))
