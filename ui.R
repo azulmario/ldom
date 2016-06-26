192a6169c5ed2a3e38da46aeb082294ae55b60f0
Mario Hernández Morales <azulmario@yahoo.com.mx>
2016-06-25 23:49:52 -0500

    Versión 20160624

--- ui.R
+++ ui.R
@@ -53,7 +53,7 @@ shinyUI(fluidPage(
         a(href = '../reporte', '/reporte.', target='_blank')),
       "Sesiones actualmente abiertas de esta aplicación:", 
       verbatimTextOutput("count"),
-      p('Versión 20160617'),
+      p('Versión 20160624'),
       textOutput("currentTime")
     ),
     mainPanel(
